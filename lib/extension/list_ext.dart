43b2db20270c7695fe72ee42e36eb778361bfebc
Whiskee <250067450@qq.com>
2025-02-24 16:46:45 +0800

    feat: Fix expansion conflicts

--- lib/extension/list_ext.dart
+++
@@ -1,9 +0,0 @@
-extension ListExt<E> on List<E> {
-
-  E? firstWhereOrNull(bool Function(E element) test) {
-    for (dynamic element in this) {
-      if (test(element)) return element;
-    }
-    return null;
-  }
-}
--- lib/flutter_ezw_index.dart
+++ lib/flutter_ezw_index.dart
@@ -10,7 +10,6 @@ export 'package:get/get.dart';
 export 'extension/double_ext.dart';
 export 'extension/duration_ext.dart';
 export 'extension/int_ext.dart';
-export 'extension/list_ext.dart';
 export 'extension/state_ext.dart';
 export 'extension/string_ext.dart';
 export 'extension/uint8list_ext.dart';
