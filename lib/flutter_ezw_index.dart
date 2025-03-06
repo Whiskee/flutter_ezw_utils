a7f446b799687d92725642f91a8cef4192ad3664
Whiskee <250067450@qq.com>
2025-03-06 10:21:01 +0800

    feat: exposed get request

--- lib/flutter_ezw_index.dart
+++ lib/flutter_ezw_index.dart
@@ -5,6 +5,7 @@ export 'package:flutter_ezw_utils/flutter_ezw_utils.dart';
 /// ThirdPart
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:get/get.dart';
+export 'package:get/get_connect/http/src/request/request.dart';
 export 'package:json_annotation/json_annotation.dart';
 
 /// Extension
