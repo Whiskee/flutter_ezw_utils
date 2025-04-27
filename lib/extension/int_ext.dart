ad8d35b0686ee5259457297bfad769049d8589fc
Whiskee <250067450@qq.com>
2025-04-27 20:19:37 +0800

    feat(extension): add Uint8List conversion method to int
    Introduce a new extension method `toUint8List` for converting integers to Uint8List with specified byte length. This enhances the utility of the `int` type by allowing easy conversion to byte arrays, which is useful for binary data manipulation.

--- lib/extension/int_ext.dart
+++ lib/extension/int_ext.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/widgets.dart';
 
 extension BytesIntExt on int {
@@ -8,6 +10,33 @@ extension BytesIntExt on int {
         (this >> 8) & 0xFF,
         this & 0xFF,
       ];
+}
+
+extension Uin8ListExt on int {
+  /// 将int转换为指定字节长度的Uint8List
+  Uint8List toUint8List(int byteLength) {
+    // 创建一个指定字节长度的ByteData
+    final byteData = ByteData(byteLength);
+    // 根据字节长度将值写入ByteData中
+    switch (byteLength) {
+      case 1:
+        byteData.setInt8(0, this);
+        break;
+      case 2:
+        byteData.setInt16(0, this, Endian.little);
+        break;
+      case 4:
+        byteData.setInt32(0, this, Endian.little);
+        break;
+      case 8:
+        byteData.setInt64(0, this, Endian.little);
+        break;
+      default:
+        throw ArgumentError('toUint8List Unsupported byte length: $byteLength');
+    }
+    // 转换ByteData为Uint8List
+    return byteData.buffer.asUint8List();
+  }
 }
 
 extension WidgetIntExt on int {
