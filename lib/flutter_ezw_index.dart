10beb539737db03ac9ed2dce1de7912cf402d0be
Whiskee <whiskee.chen@evenrealities.com>
2025-07-18 10:24:49 +0800

    refactor: reorganize exports and add date_util in flutter_ezw_index
    - Reorder exports alphabetically for better maintainability
    - Add missing date_util export

--- lib/flutter_ezw_index.dart
+++ lib/flutter_ezw_index.dart
@@ -1,6 +1,12 @@
 library flutter_ezw;
 
+export 'package:app_settings/app_settings.dart';
+export 'package:archive/archive.dart';
+export 'package:charset/charset.dart';
+export 'package:file_picker/file_picker.dart';
+export 'package:fixnum/fixnum.dart';
 export 'package:flutter_ezw_utils/flutter_ezw_utils.dart';
+export 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 /// ThirdPart
 export 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,16 +15,10 @@ export 'package:get/get.dart';
 export 'package:get/get_connect/http/src/request/request.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:lottie/lottie.dart';
+export 'package:permission_handler/permission_handler.dart';
+export 'package:scrollview_observer/scrollview_observer.dart';
 export 'package:synchronized/synchronized.dart';
 export 'package:uuid/uuid.dart';
-export 'package:file_picker/file_picker.dart';
-export 'package:app_settings/app_settings.dart';
-export 'package:permission_handler/permission_handler.dart';
-export 'package:archive/archive.dart';
-export 'package:scrollview_observer/scrollview_observer.dart';
-export 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
-export 'package:fixnum/fixnum.dart';
-export 'package:charset/charset.dart';
 
 /// Encrypt
 export 'encrypt/encrypt.dart';
@@ -28,11 +28,11 @@ export 'extension/double_ext.dart';
 export 'extension/duration_ext.dart';
 export 'extension/int16list_ext.dart';
 export 'extension/int_ext.dart';
+export 'extension/map_ext.dart';
 export 'extension/state_ext.dart';
 export 'extension/string_ext.dart';
 export 'extension/uint8list_ext.dart';
 export 'extension/widget_ext.dart';
-export 'extension/map_ext.dart';
 
 /// Json
 export 'json/unit8list_converter.dart';
@@ -42,4 +42,5 @@ export 'storage/storage.dart';
 
 /// Utils
 export 'utils/bit_util.dart';
+export 'utils/date_util.dart';
 export 'utils/file_util.dart';
