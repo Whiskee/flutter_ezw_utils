1e9a44d164b7c1c3a907c7d9b4969a0a2530e7e8
Whiskee <whiskee.chen@evenrealities.com>
2025-08-02 18:30:47 +0800

    fix: update dependencies to include path_provider and adjust debounce_throttle export

--- lib/flutter_ezw_index.dart
+++ lib/flutter_ezw_index.dart
@@ -3,6 +3,7 @@ library flutter_ezw;
 export 'package:app_settings/app_settings.dart';
 export 'package:archive/archive.dart';
 export 'package:charset/charset.dart';
+export 'package:debounce_throttle/debounce_throttle.dart';
 export 'package:file_picker/file_picker.dart';
 export 'package:fixnum/fixnum.dart';
 export 'package:flutter_ezw_utils/flutter_ezw_utils.dart';
@@ -15,11 +16,11 @@ export 'package:get/get.dart';
 export 'package:get/get_connect/http/src/request/request.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:lottie/lottie.dart';
+export 'package:path_provider/path_provider.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:scrollview_observer/scrollview_observer.dart';
 export 'package:synchronized/synchronized.dart';
 export 'package:uuid/uuid.dart';
-export 'package:debounce_throttle/debounce_throttle.dart';
 
 /// Encrypt
 export 'encrypt/encrypt.dart';
@@ -29,12 +30,12 @@ export 'extension/double_ext.dart';
 export 'extension/duration_ext.dart';
 export 'extension/int16list_ext.dart';
 export 'extension/int_ext.dart';
+export 'extension/list_ext.dart';
 export 'extension/map_ext.dart';
 export 'extension/state_ext.dart';
 export 'extension/string_ext.dart';
 export 'extension/uint8list_ext.dart';
 export 'extension/widget_ext.dart';
-export 'extension/list_ext.dart';
 
 /// Json
 export 'json/unit8list_converter.dart';
