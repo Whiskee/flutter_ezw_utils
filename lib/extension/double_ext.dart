500ad7a279a3e7f756a87e5a0a3158056a54eeaa
Whiskee <whiskee.chen@evenrealities.com>
2025-08-29 20:05:56 +0800

    feat(double_ext): add widthBox and heightBox extensions, and formatDouble methods for double precision formatting

--- lib/extension/double_ext.dart
+++ lib/extension/double_ext.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/widgets.dart';
+// ignore: depend_on_referenced_packages
+import 'package:intl/intl.dart';
 
 extension WidgetDoubleExt on double {
-  Widget get widthBox => SizedBox(width: this); 
+  //  宽度
+  Widget get widthBox => SizedBox(width: this);
+
+  //  高度
   Widget get heightBox => SizedBox(height: this);
+
+  /// 保留小数：方案1
+  String formatDouble({int precision = 1}) =>
+      this % 1 == 0 ? toInt().toString() : toStringAsFixed(precision); // 保留一位小数
+
+  /// 保留小数：方案2
+  String formatDouble2({String format = "#.#"}) =>
+      NumberFormat(format).format(this);
 }
